29c60e19be2e1371e4683d0f0f47518f0bf335bc
Jiri Lojda <jiril@MF-12-NTB.kontent.ai>
2023-03-10 16:25:36 +0100

    Split the config into three parts, rename root
    - parts:
      - index
      - jest
      - react
    - parts extend in the specified order

---
+++ jest.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    "jest",
+  ],
+
+  extends: [
+    "@kontent-ai/eslint-config",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+
+  rules: {
+    "jest/expect-expect": "error",
+    "jest/no-duplicate-hooks": "error",
+    "jest/no-test-return-statement": "error",
+    "jest/prefer-called-with": "error",
+    "jest/prefer-comparison-matcher": "error",
+    "jest/prefer-equality-matcher": "error",
+    "jest/prefer-expect-assertions": ["error", { onlyFunctionsWithAsyncKeyword: true }],
+    "jest/prefer-hooks-on-top": "error",
+    "jest/prefer-strict-equal": "error",
+    "jest/prefer-todo": "error",
+    "jest/require-top-level-describe": "error",
+
+  },
+};
---
+++ react.js
@@ -0,0 +1,79 @@
+module.exports = {
+  plugins: [
+    "react",
+    "react-hooks",
+  ],
+
+  extends: [
+    "@kontent-ai/eslint-config",
+  ],
+
+  rules: {
+    "react/hook-use-state": "error",
+    "react/jsx-boolean-value": "error",
+    "react/jsx-closing-bracket-location": "error",
+    "react/jsx-closing-tag-location": "error",
+    "react/jsx-curly-brace-presence": "error",
+    "react/jsx-curly-spacing": [
+      "error", {
+        when: "never",
+        attributes: true,
+        children: true,
+      },
+    ],
+    "react/jsx-equals-spacing": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/jsx-first-prop-new-line": ["error", "multiline"],
+    "react/jsx-fragments": "error",
+    "react/jsx-indent": [
+      "error",
+      2,
+      {
+        checkAttributes: true,
+        indentLogicalExpressions: true,
+      },
+    ],
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-max-props-per-line": "error",
+    "react/jsx-no-bind": [
+      "error",
+      {
+        ignoreRefs: true,
+        allowArrowFunctions: true,
+      },
+    ],
+    "react/jsx-no-constructed-context-values": "error",
+    "react/jsx-no-script-url": "error",
+    "react/jsx-pascal-case": "error",
+    "react/jsx-props-no-multi-spaces": "error",
+    "react/jsx-tag-spacing": "error",
+    "react/jsx-wrap-multilines": [
+      "error",
+      {
+        declaration: "parens-new-line",
+        assignment: "parens-new-line",
+        return: "parens-new-line",
+        arrow: "parens-new-line",
+        condition: "parens-new-line",
+        logical: "parens-new-line",
+        prop: "parens-new-line",
+      },
+    ],
+    "react/no-access-state-in-setstate": "error",
+    "react/no-danger": "error",
+    "react/no-redundant-should-component-update": "error",
+    "react/no-unsafe": "error",
+    "react/no-unused-state": "error",
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+    "react/no-typos": "error",
+    "react/no-this-in-sfc": "error",
+    "react/self-closing-comp": "error",
+    "react/void-dom-elements-no-children": "error",
+    "react/style-prop-object": "error",
+    "react/jsx-no-leaked-render": ["error", { validStrategies: ["coerce"] }],
+    "react/prefer-stateless-function": "error",
+
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+  },
+}
